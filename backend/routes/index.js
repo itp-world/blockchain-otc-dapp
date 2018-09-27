ba790095a75bf1c81ec0e131a5fb81e090eb7507
Siamak <ssadeghi@redhat.com>
2018-09-27 13:48:29 +0200

    added health endpoint to backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,4 +10,7 @@ module.exports = (app) => {
   app.use(`${basePath}/account`, verifyUser, require('./account'))
   app.use(`${basePath}/blockchain`, verifyUser, require('./blockchain'))
   app.use(`${basePath}/users`, verifyUser, verifyAdmin, require('./users'))
+  app.get('/health', function (req, res) {
+    res.json({status: 'UP'});
+  })
 }
