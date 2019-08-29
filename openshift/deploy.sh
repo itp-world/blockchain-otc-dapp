70c9cda8a58805d4f6f6afa65860969d8b08057d
Karsten Gresch <karsten@kgmsi01.none>
2019-08-29 11:00:20 +0200

    Removed hardcoded parity version, switched to last stable. Changed parity executable path. Tested on OCP 4.1

--- openshift/deploy.sh
+++ openshift/deploy.sh
@@ -27,6 +27,7 @@ echo "creating openshift project..."
 oc new-project $oc_project_name
 echo "creating parity pod..."
 oc create -f parity-template.yml
-oc new-app parity -p PARITY_VERSION=v1.9.6
+oc new-app parity -p PARITY_VERSION=stable
+# -p PARITY_VERSION=v1.9.6
 echo "creating client and backend..."
 oc process -f blockchain-app-template.yml -p API_URL=http://blockchain-app-backend-${oc_project_name}.${oc_wildcard_domain}/api/v1/ | oc create -f -
